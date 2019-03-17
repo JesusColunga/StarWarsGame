23e910a3bfbb1b3030d2c5ca8fbcf165c5dc071f
JesusColunga <47442182+JesusColunga@users.noreply.github.com>
2019-03-16 22:34:48 -0600

    16/Mar/2019 2234

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # unit-4-game
-CBCtec
+JavaScript Assignment - Star Wars RPG Game
+Developed by Jesus Colunga / PROAPI
+March 16th, 2019.
+
+As part of the assignments for week 4
+on the Coding BootCamp from Tec de Monterrey,
+this app was developed using the following tools:
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- Objects
+- Images:
+yodacw3principal.png
+https://www.google.com.mx/imgres?imgurl=http%3A%2F%2Fpngimg.com%2Fuploads%2Fstarwars%2Fstarwars_PNG27.png&imgrefurl=http%3A%2F%2Fpngimg.com%2Fimgs%2Ffantasy%2Fstarwars%2F&docid=TBSZWjdbWZ-6iM&tbnid=-1DyELVHOEaFQM%3A&vet=10ahUKEwir48_O3PbgAhVPXK0KHZgHBx4QMwhBKAIwAg..i&w=555&h=375&safe=active&bih=598&biw=975&q=star%20wars%20images%20png&ved=0ahUKEwir48_O3PbgAhVPXK0KHZgHBx4QMwhBKAIwAg&iact=mrc&uact=8#h=375&imgdii=Lwubw5cHWSN77M:&vet=10ahUKEwir48_O3PbgAhVPXK0KHZgHBx4QMwhBKAIwAg..i&w=555
+---
+star-wars-luke-skywalker-png-2.png
+https://www.google.com.mx/imgres?imgurl=https%3A%2F%2Fpngimage.net%2Fwp-content%2Fuploads%2F2018%2F06%2Fstar-wars-luke-skywalker-png-2.png&imgrefurl=https%3A%2F%2Fpngimage.net%2Fstar-wars-luke-skywalker-png-2%2F&docid=aSt6w7DkB-ql5M&tbnid=R01K5WKMy22lSM%3A&vet=10ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhOKBkwGQ..i&w=150&h=287&safe=active&bih=598&biw=975&q=star%20wars%20images%20png&ved=0ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhOKBkwGQ&iact=mrc&uact=8
+---
+Ahsoka_Disney_INFINITY.png
+https://www.google.com.mx/imgres?imgurl=https%3A%2F%2F3.bp.blogspot.com%2F-qGlqAECaozY%2FV55nMeKINMI%2FAAAAAAAAIJE%2FvWB0GpVjKu8O3j8FW-j0F6qP-AA5sB1ogCEw%2Fs1600%2FAhsoka_Disney_INFINITY.png&imgrefurl=http%3A%2F%2Fpgnsmundo.blogspot.com%2F2016%2F07%2Fpng-ahsoka-tano-star-wars-clone-wars.html%3Fm%3D1&docid=kzoG0hBib7LrOM&tbnid=_SFWqgmnMlSReM%3A&vet=10ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhPKBowGg..i&w=484&h=476&safe=active&bih=598&biw=975&q=star%20wars%20images%20png&ved=0ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhPKBowGg&iact=mrc&uact=8
+---
+Mace Windu
+star_wars_revenge_of_the_sith_mace_windu_png_by_metropolis_hero1125_dcanbdi-250t.png
+https://www.google.com.mx/imgres?imgurl=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Fintermediary%2Ff%2F5b8d2b12-21e8-4931-8a6d-fb9ecdd60383%2Fdcanbdi-cf1c078d-7155-4415-94a5-3e1506858a39.png%2Fv1%2Ffill%2Fw_202%2Ch_250%2Cstrp%2Fstar_wars_revenge_of_the_sith_mace_windu_png_by_metropolis_hero1125_dcanbdi-250t.png&imgrefurl=https%3A%2F%2Fwww.deviantart.com%2Fmetropolis-hero1125%2Fgallery%2F66433200%2FStar-wars-prequels-and-Clone-Wars-PNGs&docid=X9M5_IqBV2W6yM&tbnid=m2tTa8U_NxE7LM%3A&vet=10ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhTKB4wHg..i&w=202&h=250&safe=active&bih=598&biw=975&q=star%20wars%20images%20png&ved=0ahUKEwjXqqPs3fbgAhUMlawKHX2TA9UQMwhTKB4wHg&iact=mrc&uact=8
+Star wars prequels and Clone Wars PNGs by Metropolis-Hero1125 on ...
+DeviantArt
+Metropolis-Hero1125 14 0 Star Wars Revenge of the Sith Mace Windu PNG by Metropolis-Hero1125
+---
+star wars wallpaper 0.jpeg
+https://www.google.com.mx/imgres?imgurl=http%3A%2F%2F1.bp.blogspot.com%2F-_OExoEupt9g%2FViaFNMjdzbI%2FAAAAAAAAJmc%2FrDZOqpHZeMU%2Fs1600%2Fstar%252Bwars%252Bwallpaper%252B0.jpeg&imgrefurl=http%3A%2F%2Fwww.losmejoresfondosdepantalla.com%2F2015%2F10%2Fstar-wars-wallpaper-fondos-de-pantalla.html&docid=jNWy1twZOB7bBM&tbnid=lLOu1xv_u7ybJM%3A&vet=12ahUKEwjM6P--3_bgAhVCSK0KHd3ACkE4ZBAzKAEwAXoECAEQAg..i&w=1600&h=1000&safe=active&bih=598&biw=975&q=star%20wars%20fondo%20de%20pantalla&ved=2ahUKEwjM6P--3_bgAhVCSK0KHd3ACkE4ZBAzKAEwAXoECAEQAg&iact=mrc&uact=8
+Fondos para Whatsapp! | patada de caballo: Star Wars Wallpaper ...
+Fondos para Whatsapp! | patada de caballo
+star wars wallpaper
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Thanks to Elvan and M. Castillo
+for their help
+on some issues on the code.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Special thanks to 
+my wife Adriana
+and my son Michel
+for all their support
+on the day to day stuffs
+to let me "go back to school"
+to study 
+Web Development.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,87 @@
 /* game.js            */
 /* Star Wars RPG Game */
-/* 15/Mar/2019        */
+/* 16/Mar/2019        */
+
 
 // GLOBAL VARIABLES
 // =======================================================================================
 var imagesPath = "assets/images/";
+
 
 // OBJECTS
 // =======================================================================================
 var character1 = {
         name:                     "Ahsoka",
         image:                    "Ahsoka_Disney_INFINITY.png",
+        originalHP:               120,
         healthPoints:             120,
+        originalAP:               8,
         attackPower:              8,
-        counterAttackPower:       23
+        counterAttackPower:       23,
+        enabled:                  true
     };
 var character2 = {
         name:                     "Mace Windu",
         image:                    "star_wars_revenge_of_the_sith_mace_windu_png_by_metropolis_hero1125_dcanbdi-250t.png",
+        originalHP:               100,
         healthPoints:             100,
+        originalAP:               10,
         attackPower:              10,
-        counterAttackPower:       25
+        counterAttackPower:       25,
+        enabled:                  true
     };
 var character3 = {
         name:                     "Luke Skywalker",
         image:                    "star-wars-luke-skywalker-png-2.png",
+        originalHP:               150,
         healthPoints:             150,
+        originalAP:               12,
         attackPower:              12,
-        counterAttackPower:       27
+        counterAttackPower:       27,
+        enabled:                  true
     };
 var character4 = {
         name:                     "Yoda",
         image:                    "yodacw3principal.png",
+        originalHP:               180,
         healthPoints:             180,
+        originalAP:               14,
         attackPower:              14,
-        counterAttackPower:       29
+        counterAttackPower:       29,
+        enabled:                  true
     };
 
 var characters = [character1, character2, character3, character4];
 
 var game = {
-    userCharacter   : null,   // "Your Character"
-    enemies         : [],     // "Enemies Available to Attack"
-    defender        : null,   // "Defender"
+    userCharacter   : null,   // "Your Character", pointer to "characters" array
+    enemies         : [],     // "Enemies Available to Attack", array of pointers to "characters" array
+    defender        : null,   // "Defender", pointer to "characters" array
     wins            : 0,      // Number of times the usar has won
     loses           : 0,      // Number of times the usar has lost
     usrCharSelected : false,
     started         : false,
+    attacking       : false,  // When attack mode is enabled.
+    usrCharOrigAP   : 0,      // User Character original Attack Power
 
-    showCharacter1: function (area, character) {
+    ////////// ////////// //////////     Visual area     ////////// ////////// //////////
+    createImage: function (indexChar, area) {
         var image = $("<img>");
-        image.attr ("src", imagesPath + character.image);
-        image.attr ("class", "characterImage");
-        image.attr ("alt", character.name);
-        image.attr ("data-area", area);   // Is the place on screen where character will be shown.
-        $(area).append (image);
-    },
-
-    createImage: function (charImage, charName, area) {
-        var image = $("<img>");
-        image.attr ("src", imagesPath + charImage);
-        image.attr ("class", "characterImage");
-        image.attr ("alt", charName);
-        image.attr ("data-area", area);   // Is the place on screen where character will be shown.
+        image.attr ("src", imagesPath + characters [indexChar].image);
+        image.attr ("class", "characterImage"); 
+        image.attr ("alt", characters [indexChar].name);
         return image; 
-    },
+    }, 
 
-    createImgCrdFooter: function (healthPoints) {
+    createImgCrdFooter: function (indexChar) {
         imgCrdFooter = $("<div>");
         imgCrdFooter.attr ("class", "card-footer  bg-transparent  text-right");
         imgCrdFooter.attr ("style", "padding:0px 3px 4px 3px;");
-        imgCrdFooter.text (healthPoints);
+        imgCrdFooter.text ( characters [indexChar].healthPoints );
         return imgCrdFooter;
     },
 
-    createImgCrdBody:   function (charImage, charName, area) {
+    createImgCrdBody:   function (indexChar, area) {
         var backgrColor;
 
         switch (area) {
@@ -87,36 +94,39 @@ var game = {
         imgCrdBody = $("<div>");
         imgCrdBody.attr ("class", "card-body  text-center  py-1");
         imgCrdBody.attr ("style", "background:" + backgrColor); 
-        /* falta agregar img aqui: imagesPath + character.image */
-        imgCrdBody.append (game.createImage (charImage, charName, area) ); 
+        imgCrdBody.append (
+            game.createImage (indexChar, area) 
+        ); 
         return imgCrdBody;
     },
 
-    createImgCrdHeader: function (name) {
+    createImgCrdHeader: function (indexChar) {
         imgCrdHeader = $("<div>");
         imgCrdHeader.attr ("class", "card-header  bg-transparent  text-center");
         imgCrdHeader.attr ("style", "padding:3px 20px 3px 20px;  min-height: 54px;");
-        imgCrdHeader.text (name);
+        imgCrdHeader.text (characters [indexChar].name);
         return imgCrdHeader;
     },
 
-    createImageCard:    function () {
+    createImageCard:    function (indexChar) {
         imageCard = $("<div>");
         imageCard.attr ("class", "card  border-success  mr-3  mb-3  col-2");
         imageCard.attr ("style", "max-width: 9rem;  background: black;")
+        imageCard.attr ("id", "imgCard");   
+        imageCard.attr ("data-indexArrChars", indexChar);   // Pointer to the position in the array "characters".
         return imageCard;
     },
 
-    showCharacter:      function (area, character) {
+    showCharacter:      function (area, indexChar) {
         var imageCard;
         var imgCrdHeader;
         var imgCrdBody;
         var imgCrdFooter;
 
-        imageCard    = game.createImageCard     ();
-        imgCrdHeader = game.createImgCrdHeader  (character.name);
-        imgCrdBody   = game.createImgCrdBody    (character.image, character.name, area);
-        imgCrdFooter = game.createImgCrdFooter  (character.healthPoints );
+        imageCard    = game.createImageCard     (indexChar);
+        imgCrdHeader = game.createImgCrdHeader  (indexChar);
+        imgCrdBody   = game.createImgCrdBody    (indexChar, area);
+        imgCrdFooter = game.createImgCrdFooter  (indexChar);
 
         imageCard.append (imgCrdHeader);
         imageCard.append (imgCrdBody);
@@ -124,78 +134,199 @@ var game = {
 
         $(area).append (imageCard);
     },
+    ////////// ////////// //////////     -----------     ////////// ////////// //////////
 
     processUserLoses:   function () {
-        alert ("user loses");
+        $("#row12").empty ();
+        var msg1 = $("<p>");
+        msg1.text ("You have been defeated. GAME OVER !");
+        $("#row12").append (msg1);
+
+        $("#row13").empty ();
+
+        disableAttackButton ();
+        enableRestartButton ();
+    },
+
+    checkNoEnemies:     function () {
+        var noEnemies = true;
+        for (ct = 0; ct < characters.length; ct ++) {
+            if (ct != game.userCharacter) {
+                if (characters [ct].enabled) {
+                    noEnemies = false;
+                }
+            }
+        }
+        return noEnemies;
+    },
+
+    msgWonGameOver: function () {
+        $("#row12").empty ();
+        var msg1 = $("<p>");
+        msg1.text ("You Won. GAME OVER !");
+        $("#row12").append (msg1);
+
+        $("#row13").empty ();
+    },
+
+    msgFightAnotherEnemy: function () {
+        $("#row12").empty ();
+        var msg1 = $("<p>");
+        msg1.text ("You have defeated "                        +
+                   characters [game.defender].name             +
+                   ", you can choose to fight another enemy."
+                   );
+        $("#row12").append (msg1);
+
+        $("#row13").empty ();
     },
 
     processUserWins:    function () {
-        alert ("user wins");
         $("#row10").empty ();
+        disableAttackButton ();
+        characters [game.defender].enabled = false;
+        if (game.checkNoEnemies ()) {
+            game.msgWonGameOver ();
+            enableRestartButton ();
+        } else {
+            game.msgFightAnotherEnemy ();
+        }
+        game.defender = null;
+    },
+
+    reportAttack:       function () {
+        $("#row12").empty ();
+        var msg1 = $("<p>");
+        msg1.text ("You attacked "                             +
+                   characters [game.defender].name             +
+                   " for "                                     +
+                   characters [game.userCharacter].attackPower +
+                   " damage."
+                   );
+        $("#row12").append (msg1);
+
+        $("#row13").empty ();
+        var msg2 = $("<p>")
+        msg2.text (characters [game.defender].name             +
+                   " attacked you back for "                                     +
+                   characters [game.defender].counterAttackPower +
+                   " damage."
+                   );
+        $("#row13").append (msg2);
     },
 
     calculatePoints:    function () {
-        game.defender.healthPoints      -= game.userCharacter.attackPower;
-        game.userCharacter.healthPoints -= game.defender.counterAttackPower;
-        game.userCharacter.attackPower  *= 2;
+        characters [game.userCharacter].healthPoints -= characters [game.defender].counterAttackPower;
+        characters [game.defender     ].healthPoints -= characters [game.userCharacter].attackPower;
+        characters [game.userCharacter].attackPower  += game.usrCharOrigAP;
     },
 
     processAttack:      function () {
+        game.reportAttack ();
         game.calculatePoints ();
 
-        if (game.defender.healthPoints < 0) {
+        if (characters [game.defender     ].healthPoints < 0) {
             game.processUserWins ();
         } else
-        if (game.userCharacter.healthPoints < 0 ) {
+        if (characters [game.userCharacter].healthPoints < 0 ) {
             game.processUserLoses ();
+        } else {
+            $("#row4").empty ();
+            game.showCharacter ("#row4", game.userCharacter);
+
+            $("#row10").empty ();
+            game.showCharacter ("#row10", game.defender);
         }
     },
 
     restart:            function () {
-        userCharacter   = null;   // "Your Character"
-        enemies         = [];     // "Enemies Available to Attack"
-        defender        = null;   // "Defender"
-        usrCharSelected = false;
+        $("#row2").empty ();
+        $("#row4").empty ();
+        $("#row6").empty ();
+        $("#row10").empty ();
+        $("#row12").empty ();
+        $("#row13").empty ();
+        for (ct = 0; ct < characters.length; ct++) {
+            characters [ct].healthPoints = characters [ct].originalHP;
+            characters [ct].attackPower  = characters [ct].originalAP;
+            characters [ct].enabled      = true;
+        }
+        game.userCharacter   = null;   // "Your Character"
+        game.enemies         = [];     // "Enemies Available to Attack"
+        game.defender        = null;   // "Defender"
+        game.usrCharSelected = false;
+        game.attacking       = false;
+        disableRestartButton ();
+        loadAllCharacters ();   // in row 2
     }
 
-
 }  // game
+
 
 // FUNCTIONS (Definition)
 // =======================================================================================
 function loadAllCharacters () {
     for (ct=0; ct < characters.length; ct++) {
-        game.showCharacter ("#row2", characters [ct]);
+        game.showCharacter ("#row2", ct);
     }
     game.started = true;
 }
 
-function showTeams (altName) {
+function showTeams (indexChar) {
     for (ct = 0; ct < characters.length; ct++) {
-        if (characters [ct].name === altName) {
-            game.showCharacter ("#row4", characters [ct]);
-            game.userCharacter = characters [ct];
+        if (ct == indexChar) {
+            game.showCharacter ("#row4", ct);
+            game.userCharacter = ct;
             game.usrCharSelected = true;
+            game.usrCharOrigAP = characters [ct].attackPower;
         } else {
-            game.showCharacter ("#row6", characters [ct]);
-            game.enemies.push ( characters [ct]) ;
+            game.showCharacter ("#row6", ct);
+            game.enemies.push (ct);
         }
-    }  // for
+    } 
     $("#row2").empty ();
-}  // showTeams
+}  
 
 function enableAttackButton () {
     $("#attackButton").empty ();
     $("#attackButton").append ( '<button type="button" class="btn btn-danger badge-pill">  A t t a c k  </button>' );
+    game.attacking = true;
 };
 
-function showDefender (altName) {
+function disableAttackButton () {
+    $("#attackButton").empty ();
+    $("#attackButton").append ( '<button type="button" class="btn btn-dark badge-pill">  A t t a c k  </button>' );
+    game.attacking = false;
+};
+
+function enableRestartButton () {
+    $("#row14").empty ();
+    $("#row14").append ( '<button type="button" class="btn btn-primary badge-pill" id="rstBtn"> Restart Game </button>' );
+};
+
+function disableRestartButton () {
+    $("#row14").empty ();
+};
+
+
+function showDefender (indexChar) {
+    $("#row6").empty ();
+    $("#row10").empty ();
+    $("#row12").empty ();
+    $("#row13").empty ();
+
     for (ct = 0; ct < characters.length; ct++) {
-        if (characters [ct].name === altName) {
-            game.showCharacter ("#row10", characters [ct]);
-            game.defender = characters [ct];
+        if ( (ct != game.userCharacter) &&
+             (characters [ct].enabled) )  {
+            if (ct == indexChar) {
+                game.showCharacter ("#row10", ct);
+                game.defender = ct;
+            } else {
+                game.showCharacter ("#row6", ct);
+            }
         }
     }
+
     enableAttackButton ();
 }
 
@@ -207,20 +338,26 @@ $(document).ready(function() {
         loadAllCharacters ();   // in row 2
     }
 
-    $("#row2").on ("click", ".characterImage", function () {
+    $("#row2").on ("click", "#imgCard", function () {
         if (game.usrCharSelected === false) {
-            showTeams ( $(this).attr ("alt") );   // Send character name
+            showTeams ( $(this).attr ("data-indexArrChars") );      // Send index in the array of characters
         }  
     });
 
-    $("#row6").on ("click", ".characterImage", function () {
+    $("#row6").on ("click", "#imgCard", function () {
         if (game.defender === null) {
-            showDefender ( $(this).attr ("alt") );   // in row 10. Send character name.
+            showDefender ( $(this).attr ("data-indexArrChars") );   // Send index in the array of characters
         }
     });
 
     $("#attackButton").on ("click", function () {
-        game.processAttack ();
+        if (game.attacking) {
+            game.processAttack ();
+        }
+    });
+
+    $("#row14").on ("click", "#rstBtn", function () {
+        game.restart ();
     });
 
 }); // document.ready
